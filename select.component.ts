a07f2081c148eefbf8da973e0cb25dcb715f0ac4
Arthur <hlushko.arthur@gmail.com>
2022-02-19 17:22:28 +0200

    initialize

--- select.component.ts
+++ select.component.ts
@@ -6,83 +6,61 @@ import { Component, ElementRef, Input, TemplateRef, ViewChild, Output, EventEmit
 })
 export class SelectComponent {
 	@Output() update: EventEmitter<any> = new EventEmitter<any>();
+
+	@Input('placeholder') selected: string = ""
+
+	@Input() items:any = [];
+	@Input() model: any;
+	@Input() multiple: boolean = false;
+	@Input() label: string = '';
+	@Input() searchable: boolean = false;
+
+	@Output() modelChange = new EventEmitter()
+
+	public _values: any = [];
+	public _names: any = [];
+	public _selected: string;
+	public selectShow: any;
+
+
 	@Input('select') select: any = {};
-	@Input('value') value: string = 'name';
 	@Input('view') t_view: TemplateRef<any>;
 	@Input('item') t_item: TemplateRef<any>;
 	@Input('search') t_search: TemplateRef<any>;
 	public search:string = '';
 	@ViewChild('e_search', {static: false}) e_search: ElementRef;
+
 	focus_search(){
 		this.search = '';
-		if(!this.searching || this.t_search) return;
+		if(!this.searchable || this.t_search) return;
 		if (!this.e_search){
 			return setTimeout(this.focus_search.bind(this), 100);
 		}
 		this.e_search.nativeElement.focus();
 	}
-	@Input('init') init: number = 15;
-	public slice = this.init;
-	initialize() {
-		this.slice = this.init;
-		setTimeout(() => {
-			this.slice = this.items.length;
-		}, 1000);
-	}
+	
 	constructor(){}
-	@Input('searching') searching:boolean = false;
-	@Input('items') items:any = [];
-	public selectShow: any;
-	close(){
-		this.selectShow = false;
-	}
 
+	item_onclick(item) {
+		if(this.multiple) {
+			if(this._values.indexOf(item.value) != -1) {
+				this._values.splice(this._values.indexOf(item.value), 1);
+			} else
+				this._values.push(item.value);
 
-	@Input('label') label = '';
-	public counter = 0;
-	public groups = [{
-		title: 'Group 1',
-		items: [{
-			title: 'Vocation',
-			selected: true
-		}, {
-			title: 'Automotive',
-			selected: true
-		}, {
-			title: 'Education',
-			selected: false
-		}, {
-			title: 'Government',
-			selected: false
-		}]
-	}, {
-		title: 'Group 2',
-		items: [{
-			title: 'Government',
-			selected: false
-		}, {
-			title: 'Education',
-			selected: false
-		}, {
-			title: 'Automotive',
-			selected: true
-		}, {
-			title: 'Vocation',
-			selected: false
-		}]
-	}, {
-		title: 'Group 3',
-		items: [{
-			title: 'Traveling',
-			selected: false
-		}]
-	}];
-	check() {
-		this.counter = 0;
-		for (var i = 0; i < this.groups.length; i++) {
-			for (var j = 0; j < this.groups[i].items.length; j++) {
-				if (this.groups[i].items[j].selected) this.counter++;
+			if(this._names.indexOf(item.name) != -1) {
+				this._names.splice(this._names.indexOf(item.name), 1);
+			} else {
+				this._names.push(item.name);
 			}
+
+			this._selected = this._names.length == 0 ? this.selected : this._names.join(', ');
+			this.model = this._values;
+		} else {
+			this._selected = item.name;
+			this.selectShow = false;
+			this.model = item;
 		}
+		this.modelChange.emit(this.model); 
 	}
 }
