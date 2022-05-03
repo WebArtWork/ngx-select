9e9c1f9bff1732b2b34b91f2726a11ffc687ab2f
Oganesman <96392987+qweRTyiOp123789@users.noreply.github.com>
2022-05-03 17:16:28 +0300

    Add files via upload

--- select.component.ts
+++ select.component.ts
@@ -1,13 +1,14 @@
-import { Component, ElementRef, Input, TemplateRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, ElementRef, Input, TemplateRef, ViewChild, Output, EventEmitter, OnInit } from '@angular/core';
 @Component({
 	selector: 'wselect',
 	templateUrl: './select.component.html',
 	styleUrls: ['./select.component.scss']
 })
-export class SelectComponent {
+export class SelectComponent implements OnInit {
 	@Output() update: EventEmitter<any> = new EventEmitter<any>();
 	@Input('placeholder') selected: string = ""
-	@Input() items:any = [];
+	@Input() items: any = [];
+	public _items: any = {};
 	@Input() name = 'name';
 	@Input() value = '_id';
 	@Input() model: any;
@@ -23,25 +24,31 @@ export class SelectComponent {
 	@Input('view') t_view: TemplateRef<any>;
 	@Input('item') t_item: TemplateRef<any>;
 	@Input('search') t_search: TemplateRef<any>;
-	public search:string = '';
-	@ViewChild('e_search', {static: false}) e_search: ElementRef;
-	focus_search(){
+	public search: string = '';
+	@ViewChild('e_search', { static: false }) e_search: ElementRef;
+	focus_search() {
 		this.search = '';
-		if(!this.searchable || this.t_search) return;
-		if (!this.e_search){
+		if (!this.searchable || this.t_search) return;
+		if (!this.e_search) {
 			return setTimeout(this.focus_search.bind(this), 100);
 		}
 		this.e_search.nativeElement.focus();
 	}
-	constructor(){}
+	constructor() { }
+	ngOnInit() {
+		for (let i = 0; i < this.items.length; i++) {
+			this._items[this.items[i]._id] = this.items[i];
+		}
+		console.log(this._items);
+	}
 	item_onclick(item) {
-		if(this.multiple) {
-			if(this._values.indexOf(item[this.value]) != -1) {
+		if (this.multiple) {
+			if (this._values.indexOf(item[this.value]) != -1) {
 				this._values.splice(this._values.indexOf(item[this.value]), 1);
 			} else {
 				this._values.push(item[this.value]);
 			}
-			if(this._names.indexOf(item[this.name]) != -1) {
+			if (this._names.indexOf(item[this.name]) != -1) {
 				this._names.splice(this._names.indexOf(item[this.name]), 1);
 			} else {
 				this._names.push(item[this.name]);
@@ -53,6 +60,6 @@ export class SelectComponent {
 			this.selectShow = false;
 			this.model = item;
 		}
-		this.modelChange.emit(this.model); 
+		this.modelChange.emit(this.model);
 	}
 }
